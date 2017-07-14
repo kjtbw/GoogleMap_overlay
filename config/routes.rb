e4168b1d4821452e488a61fad389b99c927e02d5
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2017-07-14 10:47:30 +0200

    Change root to /map

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'map/point' => 'map#point'
   get 'map' => 'map#index'
+  root to: 'map#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
